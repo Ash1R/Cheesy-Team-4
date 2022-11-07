a360df79e8bdf96576d106cd841ca6aca651968f
Kyle-Eldridge <113394349+Kyle-Eldridge@users.noreply.github.com>
2022-11-07 15:54:36 -0800

    a

--- src/main/java/frc/robot/controls/Driver.java
+++ src/main/java/frc/robot/controls/Driver.java
@@ -3,6 +3,7 @@ package frc.robot.controls;
 import frc.robot.commands.DoNothing;
 import frc.robot.constants.Constants;
 import lib.controllers.GameController;
+import lib.controllers.GameController.Axis;
 import lib.controllers.GameController.Button;
 
 public class Driver {
@@ -10,6 +11,19 @@ public class Driver {
 
   public static void configureControls() {
     driver.get(Button.A).whenPressed(new DoNothing());
+  }
+
+  public static double getLeftX(){
+    return driver.get(Axis.LEFT_X);
+  }
+  public static double getLeftY(){
+    return driver.get(Axis.LEFT_Y);
+  }
+  public static double getRightX(){
+    return driver.get(Axis.RIGHT_X);
+  }
+  public static double getRightY(){
+    return driver.get(Axis.RIGHT_Y);
   }
 
 }
