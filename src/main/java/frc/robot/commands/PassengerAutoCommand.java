578fc95568203c001ec67c1b891da4549b210da2
Kyle-Eldridge <113394349+Kyle-Eldridge@users.noreply.github.com>
2022-11-07 16:25:34 -0800

    w

--- src/main/java/frc/robot/commands/PassengerAutoCommand.java
+++ src/main/java/frc/robot/commands/PassengerAutoCommand.java
@@ -10,9 +10,14 @@ public class PassengerAutoCommand extends SequentialCommandGroup {
     public PassengerAutoCommand(Drivetrain drive){
         addRequirements(drive);
         addCommands(
-            new InstantCommand(()->drive.tankDrive(1, 1)),
-            new WaitCommand(0.5),
-            new InstantCommand(()->drive.tankDrive(0.25, 1))
+            new InstantCommand(()->drive.tankDrive(1, 1)),//Straight to 1st location
+            new WaitCommand(0.5),//Wait until it's near/on 1st location
+            new InstantCommand(()->drive.tankDrive(0.25, 1)),//Turn left to go to 2nd location
+            new WaitCommand(0.5),//Wait until it's facing 2nd location
+            new InstantCommand(()->drive.tankDrive(1, 1)),//Go straight to go to 2nd location
+            new WaitCommand(0.25),//Wait until it's near/at 2nd location
+            new InstantCommand(()->drive.tankDrive(1, 0.5))//Turn right to go around table to 3rd location
+            //I haven't added anything else yet
         );
     }
 }
