a7b4eabb42057cdeaef5ddfc931c37ba29079e8d
Kyle-Eldridge <113394349+Kyle-Eldridge@users.noreply.github.com>
2022-11-09 15:17:33 -0800

    t

--- src/main/java/frc/robot/constants/DriveConstants.java
+++ src/main/java/frc/robot/constants/DriveConstants.java
@@ -2,9 +2,7 @@ package frc.robot.constants;
 
 public class DriveConstants {
 
-  public final int kLeftMotor1 = -1;
-  public final int kLeftMotor2 = -1;
-  public final int kRightMotor1 = -1;
-  public final int kRightMotor2 = -1;
+  public final int kLeftMotor1 = 12;
+  public final int kRightMotor1 = 13;
   
 }
