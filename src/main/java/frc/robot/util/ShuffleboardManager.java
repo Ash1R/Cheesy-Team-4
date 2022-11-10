3f26ee0a78652426b40b593fe4ee1e7ffa1c1c3f
Kyle-Eldridge <113394349+Kyle-Eldridge@users.noreply.github.com>
2022-11-09 16:03:54 -0800

    auto

--- src/main/java/frc/robot/util/ShuffleboardManager.java
+++ src/main/java/frc/robot/util/ShuffleboardManager.java
@@ -8,6 +8,8 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
+import frc.robot.Robot;
+import frc.robot.commands.PassengerAutoCommand;
 
 public class ShuffleboardManager {
 
@@ -32,6 +34,7 @@ public class ShuffleboardManager {
 
   public void chooserUpdate() {
     m_autoCommand.addOption("Do Nothing", new PrintCommand("This will do nothing!"));
+    m_autoCommand.addOption("Passenger Auto", new PassengerAutoCommand(Robot.m_drive));
   }
 
   public void loadCommandSchedulerShuffleboard(){
