b9dd38e707f606f9fa127043a0eaff8563471510
Kyle-Eldridge <113394349+Kyle-Eldridge@users.noreply.github.com>
2022-12-01 10:14:23 -0800

    out

--- src/main/java/frc/robot/constants/OuttakeConstants.java
+++ src/main/java/frc/robot/constants/OuttakeConstants.java
@@ -3,5 +3,8 @@ package frc.robot.constants;
 public class OuttakeConstants {
 
     public final int kMotorId = 0; // todo
+    public final double P = 0.0001; //todo
+    public final double I = 0; //todo
+    public final double D = 0; //todo
     
 }
--- src/main/java/frc/robot/subsystems/Outtake.java
+++ src/main/java/frc/robot/subsystems/Outtake.java
@@ -1,16 +1,58 @@
 package frc.robot.subsystems;
 
+import javax.management.MBeanAttributeInfo;
+
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.Constants;
 import frc.robot.util.MotorFactory;
 
-public class Outtake {
+public class Outtake extends SubsystemBase{
     
     private final CANSparkMax m_motor;
+    private final PIDController m_pid = new PIDController(Constants.outtake.P, Constants.outtake.I, Constants.outtake.D);
+    private boolean pidEnabled = false;
+    private double setpoint;
 
     public Outtake() {
         m_motor = MotorFactory.createSparkMAX(Constants.outtake.kMotorId, MotorType.kBrushless);
+    }
+
+    @Override
+    public void periodic(){
+        if(pidEnabled){
+            setMotor(m_pid.calculate(getMotor(), setpoint));
+        }
+    }
+
+    public void startPid(double setpoint){
+        resetPid();
+        zeroMotor();
+        setPidEnabled(true);
+        setSetpoint(setpoint);
+    }
+
+    public void zeroMotor(){
+        m_motor.getEncoder().setPosition(0);
+    }
+
+    public void resetPid(){
+        m_pid.reset();
+    }
+
+    public void setMotor(double speed){
+        m_motor.set(speed);
+    }
+    public double getMotor(){
+        return m_motor.getEncoder().getPosition();
+    }
+    public void setPidEnabled(boolean enabled){
+        pidEnabled=enabled;
+    }
+    public void setSetpoint(double setpoint){
+        this.setpoint=setpoint;
     }
 }
