b164f3bfda8fff08e6404d6706bb4a6a9299080d
Kyle-Eldridge <eldk6798@lgsstudent.org>
2022-12-11 16:25:26 -0800

    out

--- src/main/java/frc/robot/constants/OuttakeConstants.java
+++ src/main/java/frc/robot/constants/OuttakeConstants.java
@@ -2,11 +2,11 @@ package frc.robot.constants;
 
 public class OuttakeConstants {
 
-    public final int kMotorId = 0; // todo
+    public final int kMotorId = 2; // todo
     public final double P = 0.0001; //todo
     public final double I = 0; //todo
     public final double D = 0; //todo
     
     public final double openSetpoint = 0;
-    public final double closedSetpoint = 100; //todo
+    public final double closedSetpoint = 1000; //todo
 }
