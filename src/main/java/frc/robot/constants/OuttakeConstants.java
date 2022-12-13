f28f0456d9963aa14bb4c12ecd8e2313dc9bc1b6
Kyle-Eldridge <113394349+Kyle-Eldridge@users.noreply.github.com>
2022-12-13 15:05:46 -0800

    a

--- src/main/java/frc/robot/constants/OuttakeConstants.java
+++ src/main/java/frc/robot/constants/OuttakeConstants.java
@@ -7,6 +7,6 @@ public class OuttakeConstants {
     public final double I = 0; //todo
     public final double D = 0; //todo
     
-    public final double openSetpoint = 0;
-    public final double closedSetpoint = 90; //todo
+    public final double openSetpoint = 90;
+    public final double closedSetpoint = 30; //todo
 }
